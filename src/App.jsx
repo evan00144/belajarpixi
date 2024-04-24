5aca4ed9f2c85b606ec1d6564fef2a1b69913c10
Evan <evan.epifanius@gmail.com>
2024-04-24 16:20:58 +0700

    bisa geser2 pas sama mouse

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Stage, Text } from "@pixi/react";
 import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 function App() {
-	const lm = [
+	const strip3v2 = [
 		//LEFT SIDE
 		{
 			x: 25,
@@ -75,6 +75,77 @@ function App() {
 		},
 	];
 
+	const strip3v1 = [
+		//LEFT SIDE
+		{
+			width: 525,
+			height: 490,
+			x: 37,
+			y: 131,
+			photo_index: 0,
+			gravity: "center",
+		},
+		{
+			width: 525,
+			height: 490,
+			x: 37,
+			y: 628,
+			photo_index: 1,
+			gravity: "center",
+		},
+		{
+			width: 525,
+			height: 490,
+			x: 38,
+			y: 1125,
+			photo_index: 2,
+			gravity: "center",
+		},
+		//RIGHT SIDE
+		{
+			width: 525,
+			height: 490,
+			x: 638,
+			y: 131,
+			photo_index: 0,
+			gravity: "center",
+		},
+		{
+			width: 525,
+			height: 490,
+			x: 638,
+			y: 628,
+			photo_index: 1,
+			gravity: "center",
+		},
+		{
+			width: 525,
+			height: 490,
+			x: 638,
+			y: 1125,
+			photo_index: 2,
+			gravity: "center",
+		},
+		//QRIS
+		{
+			width: 95,
+			height: 95,
+			x: 445,
+			y: 1657,
+			photo_index: -1,
+			gravity: "center",
+		},
+		{
+			width: 95,
+			height: 95,
+			x: 1045,
+			y: 1657,
+			photo_index: -1,
+			gravity: "center",
+		},
+	];
+	const lm = strip3v1;
+
 	const rectangle = useCallback(
 		(g, props) => {
 			g.clear();
@@ -115,11 +186,11 @@ function App() {
 				container.eventMode = "static";
 
 				// Generate Layout
-				layout = new Graphics().rect(0, 0, 700, 700).fill(0xff700b);
+				layout = new Graphics().rect(0, 0, e.width, e.height).fill(0x808080);
 				layout.cursor = "pointer";
 				layout.eventMode = "static";
 
-				layout.on("pointerdown", onDragStart,layout);
+				layout.on("pointerdown", onDragStart, layout);
 
 				container.addChild(layout);
 				containerArray.push(container);
@@ -128,7 +199,7 @@ function App() {
 		};
 
 		const init = async () => {
-			await app.init({ width: 1200, height: 1800 });
+			await app.init({ width: 1200, height: 1800, background: "white" });
 
 			const stage = app.stage;
 
@@ -137,52 +208,70 @@ function App() {
 
 			const layoutMedia = generateMaskAndLayout();
 
-			const loadFrame = await Assets.load("/img/frame2.png");
+			const loadFrame = await Assets.load("/img/strip3v1.png");
 			const frame = new Sprite(loadFrame);
-
+			frame.eventMode = "none";
 
 			stage.eventMode = "static";
 			stage.hitArea = app.screen;
-			stage.on('pointerup', onDragEnd);
-			stage.on('pointerupoutside', onDragEnd);
+			stage.on("pointerup", onDragEnd);
+			stage.on("pointerupoutside", onDragEnd);
 
 			layoutMedia.forEach((masking, index) => {
 				stage.addChild(masking);
 			});
-
 			stage.addChild(frame);
 		};
 
-		function onDragStart() {
+		let mousePressPoint = {};
+		let dragging = true;
+
+		function onDragStart(e) {
+			let data = e.data;
+			dragging = true;
 			dragTarget = this;
-			dragTarget.alpha = 0.5;
+			data.alpha = 0.5;
+
+			console.log(data.getLocalPosition(this.parent).x)
+			console.log(this.position.x)
+			console.log(this.parent.x)
+
+			mousePressPoint.x = data.getLocalPosition(this.parent).x - this.position.x;
+			mousePressPoint.y = data.getLocalPosition(this.parent).y - this.position.y;
+
+			console.log(dragTarget);
+
 			app.stage.on("pointermove", onDragMove);
 		}
+
 		function onDragMove(event) {
-			if (dragTarget) {
-				const newX = event.global.x - dragTarget.width / 2;
-				const newY = event.global.y - dragTarget.height / 2;
+			if (dragging) {
+				let newX, newY;
+
+				// ini selalu ketika klik item nya ketengah
+				// newX = event.global.x - dragTarget.width / 2;
+				// newY = event.global.y - dragTarget.height / 2;
+
+				// ini geser ikutin mouse
+				newX = event.global.x - mousePressPoint.x;
+				newY = event.global.y - mousePressPoint.y;
+
 				const newGlobal = {
 					x: newX,
 					y: newY,
 				};
-				console.log(dragTarget);
-				console.log(event);
-				console.log(event.movement);
-				console.log(event.global);
-				// dragTarget.x = newX
-				// dragTarget.y = newY
+
 				dragTarget.parent.toLocal(newGlobal, null, dragTarget.position);
 			}
 		}
 		function onDragEnd() {
-			console.log('asd')
-			if (dragTarget) {
-				dragTarget.off("pointermove", onDragMove);
+			if (dragging && dragTarget) {
 				dragTarget.alpha = 1;
 				dragTarget = null;
+				dragging = false;
 			}
 		}
+
 		init();
 	}, []);
 
